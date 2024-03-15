692445c50c38339196ea57853c048bfe8a1e4f19
pJeyakumar <jn.piranavan@gmail.com>
2024-03-14 20:56:28 -0400

    feat: bear movement and fixing collision detection with obstacles

--- bear_maze/game.py
+++ bear_maze/game.py
@@ -9,13 +9,15 @@ FONT_SIZE = 24
 FPS = 60
 LEVEL = mazes
 COLOR = "green"
+COLOR2 = "black"
 PI = math.pi
-BEAR_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/bear_still.png'), (80,80))
+BEAR_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/bear_still.png'), (45,45))
 BEAR_START_X = 450
-BEAR_START_Y = 250
-TILE_HEIGHT = ((SCREEN_HEIGHT  - 50) // 32)
-TILE_WIDTH = (SCREEN_WIDTH // 30)
-C9_FUDGE_FACTOR = 15
+BEAR_START_Y = 300
+TILE_HEIGHT = ((SCREEN_HEIGHT  - 50) // 32) # = 32
+TILE_WIDTH = (SCREEN_WIDTH // 30) # = 64
+C9_FUDGE_FACTOR = 32
+BEAR_SPEED = 32
 
 class BearMazeGame:
   def __init__(self): 
@@ -27,7 +29,6 @@ class BearMazeGame:
     self.running = True
     self.bear_x = BEAR_START_X
     self.bear_y = BEAR_START_Y
-    self.can_turn = [False, False, False, False]
     self.direction = 0
     # self.load_assests()  
     # self.initialize_game()
@@ -41,81 +42,73 @@ class BearMazeGame:
   def draw_maze(self, level):
     for i in range(len(level)):
       for j in range(len(level[i])):
-        if level[i][j] == 1:
-          pygame.draw.line(self.screen, COLOR, (j * TILE_WIDTH + (0.5 * TILE_WIDTH), i * TILE_HEIGHT), 
-                          (j * TILE_WIDTH + (0.5 * TILE_WIDTH), i * TILE_HEIGHT + TILE_HEIGHT), 3)
-        if level[i][j] == 2:
-          pygame.draw.line(self.screen, COLOR, (j * TILE_WIDTH, i * TILE_HEIGHT + (0.5 * TILE_HEIGHT)), 
-                        (j * TILE_WIDTH + TILE_WIDTH, i * TILE_HEIGHT + (0.5 * TILE_HEIGHT)), 3)
-        if level[i][j] == 3:
-          pygame.draw.arc(self.screen, COLOR, 
-                          [(j * TILE_WIDTH + (0.5 * TILE_WIDTH)), (i * TILE_HEIGHT + (0.5 * TILE_HEIGHT)), TILE_WIDTH, TILE_HEIGHT], PI/2, PI, 3)
-        if level[i][j] == 4:
-          pygame.draw.arc(self.screen, COLOR, 
-                          [(j * TILE_WIDTH - (0.4 * TILE_WIDTH)) - 2, (i * TILE_HEIGHT + (0.5 * TILE_HEIGHT)), TILE_WIDTH, TILE_HEIGHT], 0, PI/2, 3)
-        # if level[i][j] == 5:
-        if level[i][j] == 6:
-          pygame.draw.arc(self.screen, COLOR, 
-                          [(j * TILE_WIDTH + (0.5 * TILE_WIDTH)), (i * TILE_HEIGHT - (0.4 * TILE_HEIGHT)), TILE_WIDTH, TILE_HEIGHT], PI, 3*PI/2, 3)
-        if level[i][j] == 7:
-          pygame.draw.arc(self.screen, COLOR, 
-                          [(j * TILE_WIDTH - (0.4 * TILE_WIDTH)) - 2, (i * TILE_HEIGHT - (0.4 * TILE_HEIGHT)), TILE_WIDTH, TILE_HEIGHT], 3*PI/2, 2*PI, 3)
-        if level[i][j] == 8:
-          pygame.draw.rect(self.screen, COLOR, pygame.Rect((j * TILE_WIDTH, i * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT)))
+        if level[i][j] != 0:
+          pygame.draw.rect(self.screen, COLOR2, pygame.Rect((j * TILE_WIDTH, i * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT)))
   
   def draw_bear(self):
     self.screen.blit(BEAR_IMAGE, (self.bear_x, self.bear_y))
     
   def check_position(self, level):
+    # R, L, U, D
     turns = [False, False, False, False]
-
-    if self.center_x // 30 < 29:
+    if self.center_x // TILE_WIDTH < 30 and self.center_x // TILE_WIDTH > 1:
+      # If you're currently moving RIGHT, you should be able to move LEFT, back to your initial position
       if self.direction == 0:
-        if level[self.center_y // TILE_HEIGHT][(self.center_x - C9_FUDGE_FACTOR) // TILE_WIDTH] < 3:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] == 0:
           turns[1] = True
+      # If you're currently moving LEFT, you should be able to move RIGHT, back to your initial position
       if self.direction == 1:
-        if level[self.center_y // TILE_HEIGHT][(self.center_x + C9_FUDGE_FACTOR) // TILE_WIDTH] < 3:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] == 0:
           turns[0] = True
+      # If you're currently moving UP, you should be able to move DOWN, back to your initial position
       if self.direction == 2:
-        if level[(self.center_y + C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
+        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
           turns[3] = True
+      # If you're currently moving DOWN, you should be able to move UP, back to your initial position
       if self.direction == 3:
-        if level[(self.center_y - C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
+        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
           turns[2] = True
-      
+
       if self.direction == 2 or self.direction == 3:
-        if 12 <= self.center_x % TILE_WIDTH <= 18:
-          if level[(self.center_y + C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
-            turns[3] = True
-          if level[(self.center_y - C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
-            turns[2] = True
-        if 12 <= self.center_x % TILE_HEIGHT <= 18:
-          if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] < 3:
-            turns[1] = True
-          if level[self.center_y// TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] < 3:
-            turns[0] = True
-      
+        if level[(self.center_y + C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+          turns[3] = True
+        if level[(self.center_y - C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+          turns[2] = True
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] == 0:
+          turns[1] = True
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] == 0:
+          turns[0] = True
+
       if self.direction == 0 or self.direction == 1:
-        if 12 <= self.center_x % TILE_WIDTH <= 18:
-          if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
-            turns[3] = True
-          if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] < 3:
-            turns[2] = True
-        if 12 <= self.center_x % TILE_HEIGHT <= 18:
-          if level[self.center_y // TILE_HEIGHT][(self.center_x - C9_FUDGE_FACTOR) // TILE_WIDTH] < 3:
-            turns[1] = True
-          if level[self.center_y// TILE_HEIGHT][(self.center_x + C9_FUDGE_FACTOR) // TILE_WIDTH] < 3:
-            turns[0] = True
+        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+          turns[3] = True
+        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+          turns[2] = True
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - C9_FUDGE_FACTOR) // TILE_WIDTH] == 0:
+          turns[1] = True
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + C9_FUDGE_FACTOR) // TILE_WIDTH] == 0:
+          turns[0] = True
     else:
       turns[0] = True
       turns[1] = True
-    
     return turns
 
+  def move_bear(self, bear_x, bear_y, turns_allowed):
+    if self.direction == 0 and turns_allowed[0]:
+      bear_x += BEAR_SPEED
+    elif self.direction == 1 and turns_allowed[1]:
+      bear_x -= BEAR_SPEED
+    elif self.direction == 2 and turns_allowed[2]:
+      bear_y -= BEAR_SPEED
+    elif self.direction == 3 and turns_allowed[3]:
+      bear_y += BEAR_SPEED
+    return bear_x, bear_y
+    
   def run(self):
-    while self.running: 
+    while self.running:
+      direction = -1
       self.timer.tick(FPS)
-      self.screen.fill("black")
+      self.screen.fill(COLOR)
       self.draw_maze(LEVEL)
       self.draw_bear()
       self.center_x = self.bear_x + 23
@@ -126,18 +119,18 @@ class BearMazeGame:
           self.running = False
         if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_LEFT:
-            self.direction = 1
+            direction = 1
           if event.key == pygame.K_UP:
-            self.direction = 2
+            direction = 2
           if event.key == pygame.K_RIGHT:
-            self.direction = 0
+            direction = 0
           if event.key == pygame.K_DOWN:
-            self.direction = 3
+            direction = 3
 
         for i in range(len(turns_allowed)):
-          if self.direction == i and turns_allowed[i]:
-            self.direction = 1
-
+          if direction == i and turns_allowed[i]:
+            self.direction = i
+            self.bear_x, self.bear_y = self.move_bear(self.bear_x, self.bear_y, turns_allowed)
 
       pygame.display.flip()
     
