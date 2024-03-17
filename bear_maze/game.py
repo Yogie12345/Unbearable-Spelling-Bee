fd1f4654f600cb2a4904e72752b89099113a39f6
pJeyakumar <jn.piranavan@gmail.com>
2024-03-17 19:14:07 -0400

    feat: adding arrow key graphics onto maze, changing movement controls to WASD, and allowing the bear to pass through anything that isnt an obstacle

--- bear_maze/game.py
+++ bear_maze/game.py
@@ -14,6 +14,10 @@ PI = math.pi
 BEAR_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/bear_still.png'), (45,45))
 GRASS_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/grass_block.png'), (64,32))
 CAVE_SIGN_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/cave_sign.png'), (64,32))
+ARROW_UP_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/w_arrow.png'), (64,32))
+ARROW_RIGHT_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/d_arrow.png'), (64,32))
+ARROW_DOWN_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/s_arrow.png'), (64,32))
+ARROW_LEFT_IMAGE = pygame.transform.scale(pygame.image.load(f'assets/images/maze_images/a_arrow.png'), (64,32))
 BEAR_START_X = 450
 BEAR_START_Y = 300
 TILE_HEIGHT = ((SCREEN_HEIGHT  - 50) // 32) # = 32
@@ -48,6 +52,14 @@ class BearMazeGame:
           self.screen.blit(GRASS_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
         elif level[i][j] == 2:
           self.screen.blit(CAVE_SIGN_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
+        elif level[i][j] == 3:
+           self.screen.blit(ARROW_UP_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
+        elif level[i][j] == 4:
+           self.screen.blit(ARROW_RIGHT_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
+        elif level[i][j] == 5:
+           self.screen.blit(ARROW_DOWN_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
+        elif level[i][j] == 6:
+            self.screen.blit(ARROW_LEFT_IMAGE, (j * TILE_WIDTH, i * TILE_HEIGHT))
         else:
           pygame.draw.rect(self.screen, COLOR2, pygame.Rect((j * TILE_WIDTH, i * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT)))
   
@@ -60,39 +72,39 @@ class BearMazeGame:
     if self.center_x // TILE_WIDTH < 30 and self.center_x // TILE_WIDTH > 1:
       # If you're currently moving RIGHT, you should be able to move LEFT, back to your initial position
       if self.direction == 0:
-        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] != 1:
           turns[1] = True
       # If you're currently moving LEFT, you should be able to move RIGHT, back to your initial position
       if self.direction == 1:
-        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] != 1:
           turns[0] = True
       # If you're currently moving UP, you should be able to move DOWN, back to your initial position
       if self.direction == 2:
-        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[3] = True
       # If you're currently moving DOWN, you should be able to move UP, back to your initial position
       if self.direction == 3:
-        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[2] = True
 
       if self.direction == 2 or self.direction == 3:
-        if level[(self.center_y + C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y + C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[3] = True
-        if level[(self.center_y - C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y - C9_FUDGE_FACTOR) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[2] = True
-        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - TILE_WIDTH) // TILE_WIDTH] != 1:
           turns[1] = True
-        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + TILE_WIDTH) // TILE_WIDTH] != 1:
           turns[0] = True
 
       if self.direction == 0 or self.direction == 1:
-        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y + TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[3] = True
-        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] == 0:
+        if level[(self.center_y - TILE_HEIGHT) // TILE_HEIGHT][self.center_x // TILE_WIDTH] != 1:
           turns[2] = True
-        if level[self.center_y // TILE_HEIGHT][(self.center_x - C9_FUDGE_FACTOR) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x - C9_FUDGE_FACTOR) // TILE_WIDTH] != 1:
           turns[1] = True
-        if level[self.center_y // TILE_HEIGHT][(self.center_x + C9_FUDGE_FACTOR) // TILE_WIDTH] == 0:
+        if level[self.center_y // TILE_HEIGHT][(self.center_x + C9_FUDGE_FACTOR) // TILE_WIDTH] != 1:
           turns[0] = True
     else:
       turns[0] = True
@@ -125,13 +137,13 @@ class BearMazeGame:
           self.running = False
     
       keys = pygame.key.get_pressed()
-      if keys[pygame.K_LEFT]:
+      if keys[pygame.K_a]:
         direction = 1
-      if keys[pygame.K_UP]:
+      if keys[pygame.K_w]:
         direction = 2
-      if keys[pygame.K_RIGHT]:
+      if keys[pygame.K_d]:
         direction = 0
-      if keys[pygame.K_DOWN]:
+      if keys[pygame.K_s]:
         direction = 3
 
       for i in range(len(turns_allowed)):
